578a59f499ecd20b7d022f8f6b9ed15e2e012093
Matti Paksula <matti.paksula@iki.fi>
2021-10-08 22:18:54 +0300

    :latest:

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # minercpu
 
 cpuminer wrapper
+
+    curl https://raw.githubusercontent.com/matti/minercpu/main/minercpu.sh -o /root/minercpu.sh
+    chmod +x /root/minercpu.sh
+    /root/minercpu.sh run
--- minercpu.sh
+++ minercpu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-MINERCPU_DIR=${MINERCPU_DIR:-/root/cpuminer}
+MINERCPU_CPUMINER=${MINERCPU_CPUMINER:-/root/cpuminer}
 MINERCPU_POOLS=${MINERCPU_POOLS:-stratum+tcps://stratum-eu.rplant.xyz:17056 stratum+tcp://rtm.suprnova.cc:6273 stratum+tcp://r-pool.net:3032}
 MINERCPU_RETRIES=${MINERCPU_RETRIES:-3}
 MINERCPU_ARCH=${MINERCPU_ARCH:-zen2}
@@ -12,22 +12,51 @@ _log() {
   echo "$(date) $@" | tee /tmp/minercpu.log
 }
 
-while true; do
-  for pool in $MINERCPU_POOLS; do
-    _log "using $pool"
-    for (( i=1; i<=$MINERCPU_RETRIES; i++ )); do
-      set +e
-        /root/cpuminer/cpuminer-${MINERCPU_ARCH} \
-          -a gr \
-          -o $pool \
-          -u $MINERCPU_WALLET.$MINERCPU_WORKER \
-          -d 0 \
-          --retries=0 \
-          --timeout=60
-      set -e
+case ${1:-} in
+  update)
+    curl https://raw.githubusercontent.com/matti/minercpu/main/minercpu.sh -o /root/minercpu.sh
+    chmod +x /root/minercpu.sh
+  ;;
+  install)
+    apt-get update && apt-get install \
+      libjansson-dev libnuma-dev screen
+  ;;
+  crontab)
+    old_crontab=$(crontab -l)
 
-      _log "pool failed ($i / $MINERCPU_RETRIES) $pool"
-      sleep 5
+    case $old_crontab in
+      *minercpu.sh*)
+        exit 0
+      ;;
+    esac
+
+    printf "${old_crontab}\n@reboot screen -dmS minercpu /root/minercpu.sh\n" | crontab
+  ;;
+  run)
+    $0 install
+    $0 crontab
+    exec $0
+  ;;
+  *)
+    while true; do
+      for pool in $MINERCPU_POOLS; do
+        _log "using $pool"
+        for (( i=1; i<=$MINERCPU_RETRIES; i++ )); do
+          set +e
+            $MINERCPU_CPUMINER/cpuminer-${MINERCPU_ARCH} \
+              -a gr \
+              -o $pool \
+              -u $MINERCPU_WALLET.$MINERCPU_WORKER \
+              -d 0 \
+              --retries=0 \
+              --timeout=60
+          set -e
+
+          _log "pool failed ($i / $MINERCPU_RETRIES) $pool"
+          sleep 5
+        done
+      done
     done
-  done
-done
+  ;;
+esac
+
